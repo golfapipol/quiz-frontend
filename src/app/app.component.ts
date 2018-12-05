6329142a17b0ead897e4c06d386296d228fa44f7
Apipol Sukgler <golf.apipol@gmail.com>
2018-12-05 15:14:03 +0700

    add calling backend service in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-ui';
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:4000/healtcheck')
+      .subscribe(response => console.log(response));
+  }
+
 }
